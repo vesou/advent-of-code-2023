d90d09f951971e160c9f4725836eda94036524ba
Szymon Wesolowski <szymon.wesolowski1@sainsburys.co.uk>
2023-12-09 11:24:51 +0000

    Day 4 part 2 done

--- AdventOfCode2023/Day4/Problem4.cs
+++ AdventOfCode2023/Day4/Problem4.cs
@@ -2,6 +2,57 @@ namespace AdventOfCode2023.Day4;
 
 public class Problem4
 {
+    #region Task 2
+
+    public static int Solve2()
+    {
+        var inputLines = File.ReadAllLines("Day4/input1.txt").ToList();
+        // remove Card 1: from each line
+        inputLines = inputLines.Select(x => x.Substring(x.IndexOf(":")+2)).ToList();
+        List<ScratchCardGame> scratchCardGames = TranslateInput(inputLines);
+
+        return CountOfAllScratchCardGames(scratchCardGames);
+    }
+
+    public static int CountOfAllScratchCardGames(List<ScratchCardGame> availableScratchCards)
+    {
+        int result = 0;
+        for (int i = 0; i < availableScratchCards.Count; i++)
+        {
+            PlayNextGame(availableScratchCards.ToArray(), i);
+        }
+
+        result = availableScratchCards.Sum(x => x.NumberOfCopies);
+
+        return result;
+    }
+
+    private static void PlayNextGame(ScratchCardGame[] scratchCardGames, int currentGameIndex)
+    {
+        List<int> winningNumbers = scratchCardGames[currentGameIndex].WinningNumbers;
+        List<int> myNumbers = scratchCardGames[currentGameIndex].MyNumbers;
+        int numberOfCopies = scratchCardGames[currentGameIndex].NumberOfCopies;
+
+        int result = myNumbers.Count(x => winningNumbers.Contains(x));
+
+        if (result == 0)
+        {
+            return;
+        }
+
+        AddCopiesOfGame(scratchCardGames, currentGameIndex, result, numberOfCopies);
+    }
+
+    private static void AddCopiesOfGame(ScratchCardGame[] scratchCardGames, int currentGameIndex, int scratchCardsWon, int numberOfCopies)
+    {
+        for (int i = currentGameIndex+1; i <= currentGameIndex+scratchCardsWon; i++)
+        {
+            scratchCardGames[i].NumberOfCopies += numberOfCopies;
+        }
+    }
+
+    #endregion
+
     #region Task 1
 
     public static int Solve1()
@@ -73,4 +124,24 @@ public class Problem4
     }
 
     #endregion
+
+    public static List<ScratchCardGame> TranslateInput(List<string> inputLines)
+    {
+        List<ScratchCardGame> result = new List<ScratchCardGame>();
+        foreach (var inputLine in inputLines)
+        {
+            List<int> winningNumbers = GetWinningNumbers(inputLine);
+            List<int> myNumbers = GetMyNumbers(inputLine);
+            result.Add(new ScratchCardGame(winningNumbers, myNumbers));
+        }
+
+        return result;
+    }
+}
+
+public class ScratchCardGame(List<int> winningNumbers, List<int> myNumbers, int numberOfCopies = 1)
+{
+    public List<int> WinningNumbers { get; set; } = winningNumbers;
+    public List<int> MyNumbers { get; set; } = myNumbers;
+    public int NumberOfCopies { get; set; } = numberOfCopies;
 }
--- AdventOfCode2023Tests/Problem4Tests.cs
+++ AdventOfCode2023Tests/Problem4Tests.cs
@@ -12,7 +12,7 @@ public class Problem4Tests
     [InlineData("41 92 73 84 69 | 59 84 76 51 58  5 54 83", 1)]
     [InlineData("87 83 26 28 32 | 88 30 70 12 93 22 82 36", 0)]
     [InlineData("31 18 13 56 72 | 74 77 10 23 35 67 36 11", 0)]
-    public void Solve1_SumOfParts(string input, int expectedResult)
+    public void Solve1_CalculateGamePoints(string input, int expectedResult)
     {
         var result = Problem4.CalculateGamePoints(input);
 
@@ -25,5 +25,32 @@ public class Problem4Tests
         int result = Problem4.Solve1();
 
         result.Should().Be(20855);
+    }
+
+    [Fact]
+    public void Solve2_CountOfAllScratchCardGames()
+    {
+        List<string> inputLines = new List<string>
+        {
+            "41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+            "13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+            "1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+            "41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+            "87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+            "31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+        };
+
+        List<ScratchCardGame> scratchCardGames = Problem4.TranslateInput(inputLines);
+        var result = Problem4.CountOfAllScratchCardGames(scratchCardGames);
+
+        result.Should().Be(30);
+    }
+
+    [Fact]
+    public void Solve2_FullTest()
+    {
+        int result = Problem4.Solve2();
+
+        result.Should().Be(5489600);
     }
 }
