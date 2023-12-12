e23d61fdb08226f4bfdc33362026cb5168909fd1
Szymon Wesolowski <szymon.wesolowski1@sainsburys.co.uk>
2023-12-12 20:40:59 +0000

    day 12 part 2 done

--- AdventOfCode2023/Day12/Problem12.cs
+++ AdventOfCode2023/Day12/Problem12.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace AdventOfCode2023.Day12;
 
 public class Problem12
@@ -6,34 +8,64 @@ public class Problem12
     public static char Operational { get; set; } = '.';
     public static char Damaged { get; set; } = '#';
     public static char Unknown { get; set; } = '?';
+
+    public static List<(string combination, long countOfCombinations, int dmgIndex, bool previousWasOperational)> Combinations = new();
+
     #region Task 2
 
-    public static long Solve2(int expansionSize)
+    public static long Solve2()
     {
         var inputLines = File.ReadAllLines("Day12/input1.txt").ToList();
-        List<HotSpringDamageReport> reports = TranslateInput(inputLines);
-        int result = 0;
-
+        inputLines = inputLines.Select(ExpandLine).ToList();
+        Combinations = new List<(string combination, long countOfCombinations, int dmgIndex, bool previousWasOperational)>();
+        long result = SumAllCombinations(inputLines);
         return result;
+    }
+
+    public static string ExpandLine(string line)
+    {
+        var split = line.Split(" ");
+        var hotSprings = split[0];
+        var damageNumbers = split[1];
+        var expandedLine = new StringBuilder();
+        for (int i = 0; i < 5; i++)
+        {
+            if (i < 4)
+                expandedLine.Append(hotSprings+Unknown);
+            else
+                expandedLine.Append(hotSprings);
+        }
+
+        expandedLine.Append(' ');
+        for (int i = 0; i < 5; i++)
+        {
+            if (i < 4)
+                expandedLine.Append(damageNumbers+',');
+            else
+                expandedLine.Append(damageNumbers);
+        }
+
+        return expandedLine.ToString();
     }
 
     #endregion
 
     #region Task 1
 
-    public static int Solve1()
+    public static long Solve1()
     {
         var inputLines = File.ReadAllLines("Day12/input1.txt").ToList();
-        int result = SumAllCombinations(inputLines);
+        long result = SumAllCombinations(inputLines);
         return result;
     }
 
-    private static int SumAllCombinations(List<string> inputLines)
+    private static long SumAllCombinations(List<string> inputLines)
     {
-        int result = 0;
+        long result = 0;
         var dmgReport = TranslateInput(inputLines);
         foreach (var report in dmgReport)
         {
+            Combinations = new List<(string combination, long countOfCombinations, int dmgIndex, bool previousWasOperational)>();
             var combinations = GetNumberOfCombinations(report.HotSprings, report.DamageNumbers);
             result += combinations;
         }
@@ -41,34 +73,49 @@ public class Problem12
         return result;
     }
 
-    public static int GetNumberOfCombinations(char[] hotSprings, int[] damageNumbers, int hotSpringIndex = 0, int dmgNumberIndex = 0, bool previousWasOperational = true)
+    public static long GetNumberOfCombinations(char[] hotSprings, int[] damageNumbers, int hotSpringIndex = 0, int dmgNumberIndex = 0, bool previousWasOperational = true)
     {
-        int numberOfCombinations = 0;
+        long numberOfCombinations = 0;
         if (hotSpringIndex >= hotSprings.Length)
         {
             if (dmgNumberIndex < damageNumbers.Length)
+            {
                 return 0;
+            }
 
             return 1;
         }
 
         var hotSpring = hotSprings[hotSpringIndex];
-        if (hotSpring == Operational)
+        string combination = new string(hotSprings.Skip(hotSpringIndex).ToArray());
+        if(Combinations.Exists(x => x.combination == combination && x.dmgIndex == dmgNumberIndex && x.previousWasOperational == previousWasOperational))
         {
-            numberOfCombinations += GetNumberOfCombinations(hotSprings, damageNumbers, hotSpringIndex+1, dmgNumberIndex, true);
+            numberOfCombinations += Combinations.Find(x => x.combination == combination && x.dmgIndex == dmgNumberIndex && x.previousWasOperational == previousWasOperational).countOfCombinations;
         }
-        else if (hotSpring == Damaged && DamagedIsPossible(hotSprings, damageNumbers, hotSpringIndex, dmgNumberIndex, previousWasOperational))
+        else
         {
-            numberOfCombinations += GetNumberOfCombinations(hotSprings, damageNumbers, hotSpringIndex+damageNumbers[dmgNumberIndex], dmgNumberIndex+1, false);
-        }
-        if (hotSpring == Unknown)
-        {
-            // act as operational
-            numberOfCombinations += GetNumberOfCombinations(hotSprings, damageNumbers, hotSpringIndex+1, dmgNumberIndex, true);
-            // act as damaged
-            if (DamagedIsPossible(hotSprings, damageNumbers, hotSpringIndex, dmgNumberIndex, previousWasOperational))
+            if (hotSpring == Operational)
+            {
+                numberOfCombinations += GetNumberOfCombinations(hotSprings, damageNumbers, hotSpringIndex+1, dmgNumberIndex, true);
+                Combinations.Add((combination, numberOfCombinations, dmgNumberIndex,previousWasOperational));
+            }
+            else if (hotSpring == Damaged && DamagedIsPossible(hotSprings, damageNumbers, hotSpringIndex, dmgNumberIndex, previousWasOperational))
             {
                 numberOfCombinations += GetNumberOfCombinations(hotSprings, damageNumbers, hotSpringIndex+damageNumbers[dmgNumberIndex], dmgNumberIndex+1, false);
+                Combinations.Add((combination, numberOfCombinations, dmgNumberIndex, previousWasOperational));
+            }
+            else if (hotSpring == Unknown)
+            {
+                numberOfCombinations += GetNumberOfCombinations(hotSprings, damageNumbers, hotSpringIndex+1, dmgNumberIndex, true);
+
+                // act as damaged
+                if (DamagedIsPossible(hotSprings, damageNumbers, hotSpringIndex, dmgNumberIndex, previousWasOperational))
+                {
+                    numberOfCombinations += GetNumberOfCombinations(hotSprings, damageNumbers, hotSpringIndex+damageNumbers[dmgNumberIndex], dmgNumberIndex+1, false);
+
+                }
+
+                Combinations.Add((combination, numberOfCombinations, dmgNumberIndex, previousWasOperational));
             }
         }
 
--- AdventOfCode2023Tests/Problem12Tests.cs
+++ AdventOfCode2023Tests/Problem12Tests.cs
@@ -29,4 +29,40 @@ public class Problem12Tests
 
         result.Should().Be(numberOfCombinations);
     }
+
+    [Fact]
+    public void Solve2_FullTest()
+    {
+        var result = Problem12.Solve2();
+
+        result.Should().Be(29826669191291);
+    }
+
+    [Theory]
+    [InlineData("???.### 1,1,3", 1)]
+    [InlineData(".??..??...?##. 1,1,3", 16384)]
+    [InlineData("?#?#?#?#?#?#?#? 1,3,1,6", 1)]
+    [InlineData("????.#...#... 4,1,1", 16)]
+    [InlineData("????.######..#####. 1,6,5", 2500)]
+    [InlineData("?###???????? 3,2,1", 506250)]
+    public void Solve2_GetNumberOfCombinations2(string inputLine, int numberOfCombinations)
+    {
+        List<string> inputLines = new List<string>();
+        inputLines.Add(inputLine);
+        inputLines = inputLines.Select(Problem12.ExpandLine).ToList();
+        var dmgReport = Problem12.TranslateInput(inputLines);
+        Problem12.Combinations = new List<(string combination, long countOfCombinations, int dmgIndex, bool previousWasOperational)>();
+        var result = Problem12.GetNumberOfCombinations(dmgReport[0].HotSprings, dmgReport[0].DamageNumbers);
+
+        result.Should().Be(numberOfCombinations);
+    }
+
+    [Theory]
+    [InlineData(".# 1", ".#?.#?.#?.#?.# 1,1,1,1,1")]
+    public void Solve2_ExpandLine(string inputLine, string expandedLine)
+    {
+        var result = Problem12.ExpandLine(inputLine);
+
+        result.Should().Be(expandedLine);
+    }
 }
