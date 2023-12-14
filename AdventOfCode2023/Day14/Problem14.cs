984626ebb44b4d07623738d401721115087fa000
Szymon Wesolowski <szymon.wesolowski1@sainsburys.co.uk>
2023-12-14 15:11:07 +0000

    too slow

--- AdventOfCode2023/Day14/Problem14.cs
+++ AdventOfCode2023/Day14/Problem14.cs
@@ -12,7 +12,10 @@ public class Problem14
     {
         var inputLines = File.ReadAllLines("Day14/input1.txt").ToList();
 
-        return 0;
+        RockFormation rockFormations = TranslateInput(inputLines);
+        rockFormations.Spin(1000000000);
+
+        return CalculateTotalLoad(rockFormations);
     }
 
 
@@ -84,4 +87,77 @@ public class RockFormation
     }
 
     public char[][] Rocks { get; set; }
+
+    public void Spin(int numberOfSpins)
+    {
+        for (int i = 0; i < numberOfSpins; i++)
+        {
+            Tilt(0, -1);
+            Tilt(-1, 0);
+            TiltDescendingY(0, 1);
+            TiltDescendingX(1, 0);
+        }
+    }
+
+    public void TiltDescendingY(int xChange, int yChange)
+    {
+        for (int y = Rocks.Length - 1; y >= 0; y--)
+        {
+            for (int x = 0; x < Rocks[y].Length; x++)
+            {
+                if (Rocks[y][x] == Problem14.RoundRock)
+                {
+                    MoveRoundRock(x, y, xChange, yChange);
+                }
+            }
+        }
+    }
+
+    public void TiltDescendingX(int xChange, int yChange)
+    {
+        for (int y = 0; y < Rocks.Length; y++)
+        {
+            for (int x = Rocks[y].Length - 1; x >= 0; x--)
+            {
+                if (Rocks[y][x] == Problem14.RoundRock)
+                {
+                    MoveRoundRock(x, y, xChange, yChange);
+                }
+            }
+        }
+    }
+
+    public void Tilt(int xChange, int yChange)
+    {
+        for (int y = 0; y < Rocks.Length; y++)
+        {
+            for (int x = 0; x < Rocks[y].Length; x++)
+            {
+                if (Rocks[y][x] == Problem14.RoundRock)
+                {
+                    MoveRoundRock(x, y, xChange, yChange);
+                }
+            }
+        }
+    }
+
+    private void MoveRoundRock(int x, int y, int xChange, int yChange)
+    {
+        if (x + xChange < 0 || x + xChange >= Rocks[y].Length)
+        {
+            return;
+        }
+
+        if (y + yChange < 0 || y + yChange >= Rocks.Length)
+        {
+            return;
+        }
+
+        if (Rocks[y + yChange][x + xChange] != Problem14.Space)
+            return;
+
+        Rocks[y + yChange][x + xChange] = Problem14.RoundRock;
+        Rocks[y][x] = Problem14.Space;
+        MoveRoundRock(x + xChange, y + yChange, xChange, yChange);
+    }
 }
--- AdventOfCode2023Tests/Problem14Tests.cs
+++ AdventOfCode2023Tests/Problem14Tests.cs
@@ -1,4 +1,3 @@
-using AdventOfCode2023.Day13;
 using AdventOfCode2023.Day14;
 using FluentAssertions;
 
@@ -11,7 +10,7 @@ public class Problem14Tests
     {
         var result = Problem14.Solve1();
 
-        result.Should().Be(0);
+        result.Should().Be(111339);
     }
 
     [Theory]
@@ -52,5 +51,239 @@ public class Problem14Tests
         var result = Problem14.CalculateTotalLoad(rockFormations);
 
         result.Should().Be(136);
+    }
+
+    [Fact]
+    public void Solve2_FullTest()
+    {
+        var result = Problem14.Solve2();
+
+        result.Should().Be(0);
+    }
+
+    [Fact]
+    public void Solve2_Spin()
+    {
+        List<string> inputLines = new List<string>()
+        {
+            "O....#....\nO.OO#....#\n.....##...\nOO.#O....O\n.O.....O#.\nO.#..O.#.#\n..O..#O..O\n.......O..\n#....###..\n#OO..#...."
+        };
+
+        List<string> expectedLines = new List<string>()
+        {
+            ".....#....\n....#...O#\n...OO##...\n.OO#......\n.....OOO#.\n.O#...O#.#\n....O#....\n......OOOO\n#...O###..\n#..OO#...."
+        };
+        expectedLines = expectedLines.SelectMany(x => x.Split("\n")).ToList();
+        char[][] expectedArray = expectedLines.Select(x => x.ToCharArray()).ToArray();
+
+        inputLines = inputLines.SelectMany(x => x.Split("\n")).ToList();
+        var rockFormations = Problem14.TranslateInput(inputLines);
+        rockFormations.Spin(1);
+
+        rockFormations.Rocks[0].Should().BeEquivalentTo(expectedArray[0]);
+        rockFormations.Rocks[1].Should().BeEquivalentTo(expectedArray[1]);
+        rockFormations.Rocks[2].Should().BeEquivalentTo(expectedArray[2]);
+        rockFormations.Rocks[3].Should().BeEquivalentTo(expectedArray[3]);
+        rockFormations.Rocks[4].Should().BeEquivalentTo(expectedArray[4]);
+        rockFormations.Rocks[5].Should().BeEquivalentTo(expectedArray[5]);
+        rockFormations.Rocks[6].Should().BeEquivalentTo(expectedArray[6]);
+        rockFormations.Rocks[7].Should().BeEquivalentTo(expectedArray[7]);
+        rockFormations.Rocks[8].Should().BeEquivalentTo(expectedArray[8]);
+        rockFormations.Rocks[9].Should().BeEquivalentTo(expectedArray[9]);
+        rockFormations.Rocks.Should().BeEquivalentTo(expectedArray);
+    }
+
+    [Fact]
+    public void Solve2_Tilt()
+    {
+        List<string> inputLines = new List<string>()
+        {
+            "O....#....\nO.OO#....#\n.....##...\nOO.#O....O\n.O.....O#.\nO.#..O.#.#\n..O..#O..O\n.......O..\n#....###..\n#OO..#...."
+        };
+
+        List<string> expectedLines = new List<string>()
+        {
+            "OOOO.#.O..\nOO..#....#\nOO..O##..O\nO..#.OO...\n........#.\n..#....#.#\n..O..#.O.O\n..O.......\n#....###..\n#....#...."
+        };
+        expectedLines = expectedLines.SelectMany(x => x.Split("\n")).ToList();
+        char[][] expectedArray = expectedLines.Select(x => x.ToCharArray()).ToArray();
+
+        inputLines = inputLines.SelectMany(x => x.Split("\n")).ToList();
+        var rockFormations = Problem14.TranslateInput(inputLines);
+        rockFormations.Tilt(0, -1);
+
+        rockFormations.Rocks[0].Should().BeEquivalentTo(expectedArray[0]);
+        rockFormations.Rocks[1].Should().BeEquivalentTo(expectedArray[1]);
+        rockFormations.Rocks[2].Should().BeEquivalentTo(expectedArray[2]);
+        rockFormations.Rocks[3].Should().BeEquivalentTo(expectedArray[3]);
+        rockFormations.Rocks[4].Should().BeEquivalentTo(expectedArray[4]);
+        rockFormations.Rocks[5].Should().BeEquivalentTo(expectedArray[5]);
+        rockFormations.Rocks[6].Should().BeEquivalentTo(expectedArray[6]);
+        rockFormations.Rocks[7].Should().BeEquivalentTo(expectedArray[7]);
+        rockFormations.Rocks[8].Should().BeEquivalentTo(expectedArray[8]);
+        rockFormations.Rocks[9].Should().BeEquivalentTo(expectedArray[9]);
+        rockFormations.Rocks.Should().BeEquivalentTo(expectedArray);
+    }
+
+    /*
+     * OOOO.#.O..
+       OO..#....#
+       OO..O##..O
+       O..#.OO...
+       ........#.
+       ..#....#.#
+       ..O..#.O.O
+       ..O.......
+       #....###..
+       #....#....
+
+       OOOO.#O...
+       OO..#....#
+       OOO..##O..
+       O..#OO....
+       ........#.
+       ..#....#.#
+       O....#OO..
+       O.........
+       #....###..
+       #....#....
+     */
+    [Fact]
+    public void Solve2_Tilt2()
+    {
+        List<string> inputLines = new List<string>()
+        {
+            "O....#....\nO.OO#....#\n.....##...\nOO.#O....O\n.O.....O#.\nO.#..O.#.#\n..O..#O..O\n.......O..\n#....###..\n#OO..#...."
+        };
+
+        List<string> expectedLines = new List<string>()
+        {
+            "OOOO.#O...\nOO..#....#\nOOO..##O..\nO..#OO....\n........#.\n..#....#.#\nO....#OO..\nO.........\n#....###..\n#....#...."
+        };
+        expectedLines = expectedLines.SelectMany(x => x.Split("\n")).ToList();
+        char[][] expectedArray = expectedLines.Select(x => x.ToCharArray()).ToArray();
+
+        inputLines = inputLines.SelectMany(x => x.Split("\n")).ToList();
+        var rockFormations = Problem14.TranslateInput(inputLines);
+        rockFormations.Tilt(0, -1);
+        rockFormations.Tilt(-1, 0);
+
+        rockFormations.Rocks[0].Should().BeEquivalentTo(expectedArray[0]);
+        rockFormations.Rocks[1].Should().BeEquivalentTo(expectedArray[1]);
+        rockFormations.Rocks[2].Should().BeEquivalentTo(expectedArray[2]);
+        rockFormations.Rocks[3].Should().BeEquivalentTo(expectedArray[3]);
+        rockFormations.Rocks[4].Should().BeEquivalentTo(expectedArray[4]);
+        rockFormations.Rocks[5].Should().BeEquivalentTo(expectedArray[5]);
+        rockFormations.Rocks[6].Should().BeEquivalentTo(expectedArray[6]);
+        rockFormations.Rocks[7].Should().BeEquivalentTo(expectedArray[7]);
+        rockFormations.Rocks[8].Should().BeEquivalentTo(expectedArray[8]);
+        rockFormations.Rocks[9].Should().BeEquivalentTo(expectedArray[9]);
+        rockFormations.Rocks.Should().BeEquivalentTo(expectedArray);
+    }
+
+     /*
+     *  OOOO.#O...
+        OO..#....#
+        OOO..##O..
+        O..#OO....
+        ........#.
+        ..#....#.#
+        O....#OO..
+        O.........
+        #....###..
+        #....#....
+
+       .....#....
+       ....#.O..#
+       O..O.##...
+       O.O#......
+       O.O....O#.
+       O.#..O.#.#
+       O....#....
+       OO....OO..
+       #O...###..
+       #O..O#....
+     */
+    [Fact]
+    public void Solve2_Tilt3()
+    {
+        List<string> inputLines = new List<string>()
+        {
+            "O....#....\nO.OO#....#\n.....##...\nOO.#O....O\n.O.....O#.\nO.#..O.#.#\n..O..#O..O\n.......O..\n#....###..\n#OO..#...."
+        };
+
+        List<string> expectedLines = new List<string>()
+        {
+            ".....#....\n....#.O..#\nO..O.##...\nO.O#......\nO.O....O#.\nO.#..O.#.#\nO....#....\nOO....OO..\n#O...###..\n#O..O#...."
+        };
+        expectedLines = expectedLines.SelectMany(x => x.Split("\n")).ToList();
+        char[][] expectedArray = expectedLines.Select(x => x.ToCharArray()).ToArray();
+
+        inputLines = inputLines.SelectMany(x => x.Split("\n")).ToList();
+        var rockFormations = Problem14.TranslateInput(inputLines);
+        rockFormations.Tilt(0, -1);
+        rockFormations.Tilt(-1, 0);
+        rockFormations.TiltDescendingY(0, 1);
+
+        rockFormations.Rocks[0].Should().BeEquivalentTo(expectedArray[0]);
+        rockFormations.Rocks[1].Should().BeEquivalentTo(expectedArray[1]);
+        rockFormations.Rocks[2].Should().BeEquivalentTo(expectedArray[2]);
+        rockFormations.Rocks[3].Should().BeEquivalentTo(expectedArray[3]);
+        rockFormations.Rocks[4].Should().BeEquivalentTo(expectedArray[4]);
+        rockFormations.Rocks[5].Should().BeEquivalentTo(expectedArray[5]);
+        rockFormations.Rocks[6].Should().BeEquivalentTo(expectedArray[6]);
+        rockFormations.Rocks[7].Should().BeEquivalentTo(expectedArray[7]);
+        rockFormations.Rocks[8].Should().BeEquivalentTo(expectedArray[8]);
+        rockFormations.Rocks[9].Should().BeEquivalentTo(expectedArray[9]);
+        rockFormations.Rocks.Should().BeEquivalentTo(expectedArray);
+    }
+
+    [Fact]
+    public void Solve2_Spin3()
+    {
+        List<string> inputLines = new List<string>()
+        {
+            "O....#....\nO.OO#....#\n.....##...\nOO.#O....O\n.O.....O#.\nO.#..O.#.#\n..O..#O..O\n.......O..\n#....###..\n#OO..#...."
+        };
+
+        List<string> expectedLines = new List<string>()
+        {
+            ".....#....\n....#...O#\n.....##...\n..O#......\n.....OOO#.\n.O#...O#.#\n....O#...O\n.......OOO\n#...O###.O\n#.OOO#...O"
+        };
+        expectedLines = expectedLines.SelectMany(x => x.Split("\n")).ToList();
+        char[][] expectedArray = expectedLines.Select(x => x.ToCharArray()).ToArray();
+
+        inputLines = inputLines.SelectMany(x => x.Split("\n")).ToList();
+        var rockFormations = Problem14.TranslateInput(inputLines);
+        rockFormations.Spin(3);
+
+        rockFormations.Rocks[0].Should().BeEquivalentTo(expectedArray[0]);
+        rockFormations.Rocks[1].Should().BeEquivalentTo(expectedArray[1]);
+        rockFormations.Rocks[2].Should().BeEquivalentTo(expectedArray[2]);
+        rockFormations.Rocks[3].Should().BeEquivalentTo(expectedArray[3]);
+        rockFormations.Rocks[4].Should().BeEquivalentTo(expectedArray[4]);
+        rockFormations.Rocks[5].Should().BeEquivalentTo(expectedArray[5]);
+        rockFormations.Rocks[6].Should().BeEquivalentTo(expectedArray[6]);
+        rockFormations.Rocks[7].Should().BeEquivalentTo(expectedArray[7]);
+        rockFormations.Rocks[8].Should().BeEquivalentTo(expectedArray[8]);
+        rockFormations.Rocks[9].Should().BeEquivalentTo(expectedArray[9]);
+        rockFormations.Rocks.Should().BeEquivalentTo(expectedArray);
+    }
+
+    [Fact]
+    public void Solve2_SpinLoads()
+    {
+        List<string> inputLines = new List<string>()
+        {
+            "O....#....\nO.OO#....#\n.....##...\nOO.#O....O\n.O.....O#.\nO.#..O.#.#\n..O..#O..O\n.......O..\n#....###..\n#OO..#...."
+        };
+
+        inputLines = inputLines.SelectMany(x => x.Split("\n")).ToList();
+        var rockFormations = Problem14.TranslateInput(inputLines);
+        rockFormations.Spin(1000000000);
+
+        var result = Problem14.CalculateTotalLoad(rockFormations);
+
+        result.Should().Be(64);
     }
 }
