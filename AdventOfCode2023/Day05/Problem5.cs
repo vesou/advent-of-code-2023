bd33cae8f2be0f44145f15433d0fb41e1921f6db
Szymon Wesolowski <szymon.wesolowski1@sainsburys.co.uk>
2023-12-14 12:07:00 +0000

    Day 5 part 1

--- AdventOfCode2023/Day05/Problem5.cs
+++ AdventOfCode2023/Day05/Problem5.cs
@@ -4,39 +4,187 @@ public class Problem5
 {
     #region Task 2
 
-    public static int Solve2()
+    public static long Solve2()
     {
         var inputLines = File.ReadAllLines("Day05/input1.txt").ToList();
+        var allData = TranslateInput2(inputLines);
+        var result = allData.FindLowestLocation();
 
-        return 0;
+        return result;
     }
 
+    public static Data TranslateInput2(List<string> inputLines)
+    {
+        Data data = new Data();
+        data.Seeds = inputLines[0].Substring("seeds: ".Length).Split(" ").Select(long.Parse).ToList();
+        data.SeedRanges = new List<(long, long)>();
+        for (int i = 0; i < data.Seeds.Count; i+=2)
+        {
+            data.SeedRanges.Add((data.Seeds[i], data.Seeds[i + 1]));
+        }
 
+        int indexOfSeedToSoil = inputLines.IndexOf("seed-to-soil map:");
+        int indexOfSoilToFertilizer = inputLines.IndexOf("soil-to-fertilizer map:");
+        int indexOfFertilizerToWater = inputLines.IndexOf("fertilizer-to-water map:");
+        int indexOfWaterToLight = inputLines.IndexOf("water-to-light map:");
+        int indexOfLightToTemperature = inputLines.IndexOf("light-to-temperature map:");
+        int indexOfTemperatureToHumidity = inputLines.IndexOf("temperature-to-humidity map:");
+        int indexOfHumidityToLocation = inputLines.IndexOf("humidity-to-location map:");
+        data.SeedToSoil = GetRuleSet(inputLines, indexOfSeedToSoil, indexOfSoilToFertilizer);
+        data.SoilToFertilizer = GetRuleSet(inputLines, indexOfSoilToFertilizer, indexOfFertilizerToWater);
+        data.FertilizerToWater = GetRuleSet(inputLines, indexOfFertilizerToWater, indexOfWaterToLight);
+        data.WaterToLight = GetRuleSet(inputLines, indexOfWaterToLight, indexOfLightToTemperature);
+        data.LightToTemperature = GetRuleSet(inputLines, indexOfLightToTemperature, indexOfTemperatureToHumidity);
+        data.TemperatureToHumidity = GetRuleSet(inputLines, indexOfTemperatureToHumidity, indexOfHumidityToLocation);
+        data.HumidityToLocation = GetRuleSet(inputLines, indexOfHumidityToLocation, inputLines.Count);
+
+        return data;
+    }
 
     #endregion
 
     #region Task 1
 
-    public static int Solve1()
+    public static long Solve1()
     {
         var inputLines = File.ReadAllLines("Day05/input1.txt").ToList();
+        var allData = TranslateInput(inputLines);
+        allData.TranslateAllSeedsToLocations();
 
-        return 0;
+        return GetLowestLocation(allData);
     }
 
+    private static long GetLowestLocation(Data data)
+    {
+        return data.SeedsToLocation.Min();
+    }
 
     #endregion
 
-    public static List<Data> TranslateInput(List<string> inputLines)
+    public static Data TranslateInput(List<string> inputLines)
     {
+        Data data = new Data();
+        data.Seeds = inputLines[0].Substring("seeds: ".Length).Split(" ").Select(long.Parse).ToList();
+        data.SeedToSoil = new RuleSet();
+        int indexOfSeedToSoil = inputLines.IndexOf("seed-to-soil map:");
+        int indexOfSoilToFertilizer = inputLines.IndexOf("soil-to-fertilizer map:");
+        int indexOfFertilizerToWater = inputLines.IndexOf("fertilizer-to-water map:");
+        int indexOfWaterToLight = inputLines.IndexOf("water-to-light map:");
+        int indexOfLightToTemperature = inputLines.IndexOf("light-to-temperature map:");
+        int indexOfTemperatureToHumidity = inputLines.IndexOf("temperature-to-humidity map:");
+        int indexOfHumidityToLocation = inputLines.IndexOf("humidity-to-location map:");
+        data.SeedToSoil = GetRuleSet(inputLines, indexOfSeedToSoil, indexOfSoilToFertilizer);
+        data.SoilToFertilizer = GetRuleSet(inputLines, indexOfSoilToFertilizer, indexOfFertilizerToWater);
+        data.FertilizerToWater = GetRuleSet(inputLines, indexOfFertilizerToWater, indexOfWaterToLight);
+        data.WaterToLight = GetRuleSet(inputLines, indexOfWaterToLight, indexOfLightToTemperature);
+        data.LightToTemperature = GetRuleSet(inputLines, indexOfLightToTemperature, indexOfTemperatureToHumidity);
+        data.TemperatureToHumidity = GetRuleSet(inputLines, indexOfTemperatureToHumidity, indexOfHumidityToLocation);
+        data.HumidityToLocation = GetRuleSet(inputLines, indexOfHumidityToLocation, inputLines.Count);
 
-        return new List<Data>();
+        return data;
+    }
+
+    private static RuleSet GetRuleSet(List<string> inputLines, int startIndex, int endIndex)
+    {
+        RuleSet ruleSet = new RuleSet();
+        for (int i = startIndex + 1; i < endIndex - 1; i++)
+        {
+            var rule = GetRule(inputLines[i]);
+            ruleSet.Rules.Add(rule);
+        }
+
+        ruleSet.Rules = ruleSet.Rules.OrderBy(x => x.Source).ToList();
+
+        return ruleSet;
+    }
+
+    private static Rule GetRule(string inputLine)
+    {
+        var parts = inputLine.Split(" ");
+        return new Rule()
+        {
+            Source = long.Parse(parts[1]),
+            Destination = long.Parse(parts[0]),
+            Count = long.Parse(parts[2])
+        };
     }
 }
 
 public class Data()
 {
-    public List<int> WinningNumbers { get; set; }
-    public List<int> MyNumbers { get; set; }
-    public int NumberOfCopies { get; set; }
+    public List<long> Seeds { get; set; }
+    public List<(long, long)> SeedRanges { get; set; }
+    public List<long> SeedsToLocation { get; set; }
+    public RuleSet SeedToSoil { get; set; }
+    public RuleSet SoilToFertilizer { get; set; }
+    public RuleSet FertilizerToWater { get; set; }
+    public RuleSet WaterToLight { get; set; }
+    public RuleSet LightToTemperature { get; set; }
+    public RuleSet TemperatureToHumidity { get; set; }
+    public RuleSet HumidityToLocation { get; set; }
+
+    public void TranslateAllSeedsToLocations()
+    {
+        SeedsToLocation = Seeds.Select(TranslateSeedToLocation).ToList();
+    }
+
+    public long FindLowestLocation()
+    {
+        long lowestLocation = long.MaxValue;
+
+        return lowestLocation;
+    }
+
+    public long TranslateSeedToLocation(long seedNumber)
+    {
+        seedNumber = SeedToSoil.Transform(seedNumber);
+        seedNumber = SoilToFertilizer.Transform(seedNumber);
+        seedNumber = FertilizerToWater.Transform(seedNumber);
+        seedNumber = WaterToLight.Transform(seedNumber);
+        seedNumber = LightToTemperature.Transform(seedNumber);
+        seedNumber = TemperatureToHumidity.Transform(seedNumber);
+        seedNumber = HumidityToLocation.Transform(seedNumber);
+
+        return seedNumber;
+    }
+}
+
+public class RuleSet
+{
+    public List<Rule> Rules { get; set; } = new List<Rule>();
+
+    public long Transform(long input)
+    {
+        if(input < Rules[0].Source)
+            return input;
+
+        if(input > Rules[^1].Source + Rules[^1].Count)
+            return input;
+
+        Rule impactingRule = FindRule(input);
+
+        return impactingRule.Destination + (input - impactingRule.Source);
+    }
+
+    private Rule FindRule(long input)
+    {
+        foreach (var rule in Rules)
+        {
+            if(rule.Source <= input && rule.Source + rule.Count > input)
+                return rule;
+        }
+
+        return new Rule()
+        {
+            Source = 0,
+            Destination = 0,
+            Count = 0
+        };
+    }
+}
+public class Rule
+{
+    public long Source { get; set; }
+    public long Destination { get; set; }
+    public long Count { get; set; }
 }
--- AdventOfCode2023Tests/Problem5Tests.cs
+++ AdventOfCode2023Tests/Problem5Tests.cs
@@ -1,4 +1,6 @@
 using AdventOfCode2023.Day04;
+using AdventOfCode2023.Day05;
+using AdventOfCode2023.Day12;
 using FluentAssertions;
 
 namespace AdventOfCode2023Tests;
@@ -6,24 +8,71 @@ namespace AdventOfCode2023Tests;
 public class Problem5Tests
 {
     [Theory]
-    [InlineData("41 48 83 86 17 | 83 86  6 31 17  9 48 53", 8)]
-    [InlineData("13 32 20 16 61 | 61 30 68 82 17 32 24 19", 2)]
-    [InlineData("1 21 53 59 44 | 69 82 63 72 16 21 14  1", 2)]
-    [InlineData("41 92 73 84 69 | 59 84 76 51 58  5 54 83", 1)]
-    [InlineData("87 83 26 28 32 | 88 30 70 12 93 22 82 36", 0)]
-    [InlineData("31 18 13 56 72 | 74 77 10 23 35 67 36 11", 0)]
-    public void Solve1_CalculateGamePoints(string input, int expectedResult)
+    [InlineData(79, 81)]
+    [InlineData(14, 14)]
+    [InlineData(55, 57)]
+    [InlineData(13, 13)]
+    public void Solve1_TranslateSeedToSoil(int source, int destination)
     {
-        var result = Problem4.CalculateGamePoints(input);
+        List<string> inputLines = "seeds: 79 14 55 13\n\nseed-to-soil map:\n50 98 2\n52 50 48\n\nsoil-to-fertilizer map:\n0 15 37\n37 52 2\n39 0 15\n\nfertilizer-to-water map:\n49 53 8\n0 11 42\n42 0 7\n57 7 4\n\nwater-to-light map:\n88 18 7\n18 25 70\n\nlight-to-temperature map:\n45 77 23\n81 45 19\n68 64 13\n\ntemperature-to-humidity map:\n0 69 1\n1 0 69\n\nhumidity-to-location map:\n60 56 37\n56 93 4"
+            .Split("\n")
+            .ToList();
+        var data = Problem5.TranslateInput(inputLines);
+        var result = data.SeedToSoil.Transform(source);
 
-        result.Should().Be(expectedResult);
+        result.Should().Be(destination);
+    }
+
+    [Theory]
+    [InlineData(81, 81)]
+    [InlineData(14, 53)]
+    [InlineData(57, 57)]
+    [InlineData(13, 52)]
+    public void Solve1_TranslateSoilToFertilizer(int source, int destination)
+    {
+        List<string> inputLines = "seeds: 79 14 55 13\n\nseed-to-soil map:\n50 98 2\n52 50 48\n\nsoil-to-fertilizer map:\n0 15 37\n37 52 2\n39 0 15\n\nfertilizer-to-water map:\n49 53 8\n0 11 42\n42 0 7\n57 7 4\n\nwater-to-light map:\n88 18 7\n18 25 70\n\nlight-to-temperature map:\n45 77 23\n81 45 19\n68 64 13\n\ntemperature-to-humidity map:\n0 69 1\n1 0 69\n\nhumidity-to-location map:\n60 56 37\n56 93 4"
+            .Split("\n")
+            .ToList();
+        var data = Problem5.TranslateInput(inputLines);
+        var result = data.SoilToFertilizer.Transform(source);
+
+        result.Should().Be(destination);
+    }
+
+    [Theory]
+    [InlineData(81, 81)]
+    [InlineData(53, 49)]
+    [InlineData(57, 53)]
+    [InlineData(52, 41)]
+    public void Solve1_TranslateFertilizerToWater(int source, int destination)
+    {
+        List<string> inputLines = "seeds: 79 14 55 13\n\nseed-to-soil map:\n50 98 2\n52 50 48\n\nsoil-to-fertilizer map:\n0 15 37\n37 52 2\n39 0 15\n\nfertilizer-to-water map:\n49 53 8\n0 11 42\n42 0 7\n57 7 4\n\nwater-to-light map:\n88 18 7\n18 25 70\n\nlight-to-temperature map:\n45 77 23\n81 45 19\n68 64 13\n\ntemperature-to-humidity map:\n0 69 1\n1 0 69\n\nhumidity-to-location map:\n60 56 37\n56 93 4"
+            .Split("\n")
+            .ToList();
+        var data = Problem5.TranslateInput(inputLines);
+        var result = data.FertilizerToWater.Transform(source);
+
+        result.Should().Be(destination);
+    }
+
+    [Fact]
+    public void Solve1_TranslateAllSeedsToLocations()
+    {
+        List<string> inputLines = "seeds: 79 14 55 13\n\nseed-to-soil map:\n50 98 2\n52 50 48\n\nsoil-to-fertilizer map:\n0 15 37\n37 52 2\n39 0 15\n\nfertilizer-to-water map:\n49 53 8\n0 11 42\n42 0 7\n57 7 4\n\nwater-to-light map:\n88 18 7\n18 25 70\n\nlight-to-temperature map:\n45 77 23\n81 45 19\n68 64 13\n\ntemperature-to-humidity map:\n0 69 1\n1 0 69\n\nhumidity-to-location map:\n60 56 37\n56 93 4"
+            .Split("\n")
+            .ToList();
+        var result = Problem5.TranslateInput(inputLines);
+        result.TranslateAllSeedsToLocations();
+
+        result.SeedsToLocation.Should().BeEquivalentTo(new List<long> { 82, 43, 86, 35 });
     }
 
     [Fact]
     public void Solve1_FullTest()
     {
-        int result = Problem4.Solve1();
+        long result = Problem5.Solve1();
 
-        result.Should().Be(20855);
+        result.Should().BeLessThan(218367919);
+        result.Should().Be(51580674);
     }
 }
